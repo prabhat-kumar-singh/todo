6b32bca9ab6065449e79e9226fc720d2b18bc3e6
Prabhat <prabhatkumarsingh462@gmail.com>
2021-01-16 01:50:05 +0530

    Router b/w sign in and Login

--- mytodo/src/App.js
+++ mytodo/src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//auth component
+import Login from "./authComponent/Login";
+import Signin from "./authComponent/Signin";
+
 const App = () => {
     return(
         <div>
-            Hey There!
+            <div>{/* SVG Image Here */}</div>
+            <div>
+                <Router>
+                    <Switch>
+                        <Route path = "/" exact> <Signin /> </Route>
+                        <Route path = "/login"> <Login /> </Route>
+                    </Switch>
+                </Router>
+            </div>
         </div>
     );
 }
