17f4b8e3d5219ffa739f27e5e19b7476eb34e851
Prabhat <prabhatkumarsingh462@gmail.com>
2021-01-16 00:03:32 +0530

    Deleting all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# todo
-A todo application with reactjs and firebase database. It also includes some features like getting weather and programming quizzes.
--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
-import Authentication from "./component/Authentication";
-
-ReactDOM.render(
-  <Authentication />,
-  document.getElementById('root')
-);
