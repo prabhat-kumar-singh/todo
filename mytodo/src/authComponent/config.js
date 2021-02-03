31d36f8efa560b56a7b10b2a9cce84edb8d97aa9
prabhat-kumar-singh <51071075+prabhat-kumar-singh@users.noreply.github.com>
2021-02-03 15:24:14 +0530

    Update config.js

--- mytodo/src/authComponent/config.js
+++ mytodo/src/authComponent/config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-EE-De4Bbj3xcN2y9oa8SYzrGr3Gy6cM",
-    authDomain: "mytodo-4e8dc.firebaseapp.com",
-    projectId: "mytodo-4e8dc",
-    storageBucket: "mytodo-4e8dc.appspot.com",
-    messagingSenderId: "1071057257100",
-    appId: "1:1071057257100:web:fbffc8551f0410810531a9",
-    measurementId: "G-167HZPB32S"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 const FirebaseApp = firebase.initializeApp(firebaseConfig);
